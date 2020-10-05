95a1c0cb178255b9bd1bec087518f8d468875767
vperezm <valeperezmog@gmail.com>
2020-10-05 15:37:54 -0300

    syntax for Eval1.hs, added test example

--- TP1/src/Eval1.hs
+++ TP1/src/Eval1.hs
@@ -6,7 +6,7 @@ where
 
 import           AST
 import qualified Data.Map.Strict               as M
-import           Data.Strict.Tuple             as T
+import           Data.Strict.Tuple
 
 -- Estados
 type State = M.Map Variable Int
@@ -37,29 +37,60 @@ stepCommStar c    s = T.uncurry stepCommStar $ stepComm c s
 -- Evalúa un paso de un comando en un estado dado
 stepComm :: Comm -> State -> Pair Comm State
 stepComm Skip                 s = (Skip :!: s)
-stepComm (Let v e)            s = (Skip :!: update v (T.fst (evalExp e s)) (T.snd (evalExp e s)))
+stepComm (Let v e)            s = let (n :!: s') = evalExp e s
+                                  in (Skip :!: update v n s')
 stepComm (Seq Skip c1)        s = (c1 :!: s)
-stepComm (Seq c0 c1)          s = (Seq (T.fst (stepComm c0 s)) c1 :!: (T.snd (stepComm c0 s)))
-stepComm (IfThenElse b c0 c1) s = if (T.fst (evalExp b s)) then (c0 :!: (T.snd (evalExp b s))) else (c1 :!: (T.snd (evalExp b s)))
-stepComm (While b c)          s = if (T.fst (evalExp b s)) then ((Seq c (While b c)) :!: (T.snd (evalExp b s))) else (Skip :!: (T.snd (evalExp b s)))
+stepComm (Seq c0 c1)          s = let (c0' :!: s') = stepComm c0 s
+                                  in (Seq c0' c1 :!: s')
+stepComm (IfThenElse b c0 c1) s = let (b' :!: s') = evalExp b s
+                                  in if b' then (c0 :!: s') else (c1 :!: s')
+stepComm (While b c)          s = let (b' :!: s') = evalExp b s
+                                  in if b' then (Seq c (While b c) :!: s') else (Skip :!: s')
 
 -- Evalúa una expresión
 evalExp :: Exp a -> State -> Pair a State
+-- Expresiones enteras
 evalExp (Const n)     s = (n :!: s)
 evalExp (Var v)       s = ((lookfor v s) :!: s)
-evalExp (UMinus e)    s = (-(T.fst (evalExp e s)) :!: T.snd (evalExp e s))
-evalExp (Plus e0 e1)  s = ((T.fst (evalExp e0 s)) + (T.fst (evalExp e1 (T.snd (evalExp e0 s)))) :!: T.snd (evalExp e1 (T.snd (evalExp e0 s))))
-evalExp (Minus e0 e1) s = ((T.fst (evalExp e0 s)) - (T.fst (evalExp e1 (T.snd (evalExp e0 s)))) :!: T.snd (evalExp e1 (T.snd (evalExp e0 s))))
-evalExp (Times e0 e1) s = ((T.fst (evalExp e0 s)) * (T.fst (evalExp e1 (T.snd (evalExp e0 s)))) :!: T.snd (evalExp e1 (T.snd (evalExp e0 s))))
-evalExp (Div e0 e1)   s = ((T.fst (evalExp e0 s)) `div` (T.fst (evalExp e1 (T.snd (evalExp e0 s)))) :!: T.snd (evalExp e1 (T.snd (evalExp e0 s))))
-evalExp (EAssgn v e)  s = (T.fst (evalExp e s) :!: update v (T.fst (evalExp e s)) (T.snd (evalExp e s)))
-evalExp (ESeq e0 e1)  s = evalExp e1 (T.snd (evalExp e0 s))
+evalExp (UMinus e)    s = let (n :!: s') = evalExp e s
+                          in (-n :!: s')
+evalExp (Plus e0 e1)  s = let (n0 :!: s')  = evalExp e0 s
+                              (n1 :!: s'') = evalExp e1 s'
+                          in (n0 + n1 :!: s'')
+evalExp (Minus e0 e1) s = let (n0 :!: s')  = evalExp e0 s
+                              (n1 :!: s'') = evalExp e1 s'
+                          in (n0 - n1 :!: s'')
+evalExp (Times e0 e1) s = let (n0 :!: s')  = evalExp e0 s
+                              (n1 :!: s'') = evalExp e1 s'
+                          in (n0 * n1 :!: s'')
+evalExp (Div e0 e1)   s = let (n0 :!: s')  = evalExp e0 s
+                              (n1 :!: s'') = evalExp e1 s'
+                          in (n0 `div` n1 :!: s'')
+evalExp (EAssgn v e)  s = let (n :!: s') = evalExp e s
+                          in (n :!: update v n s')
+evalExp (ESeq e0 e1)  s = let (n0 :!: s')  = evalExp e0 s
+                              (n1 :!: s'') = evalExp e1 s'
+                          in (n1 :!: s'')
+-- Expresiones booleanas
 evalExp BTrue         s = (True :!: s)
 evalExp BFalse        s = (False :!: s)
-evalExp (Lt e0 e1)    s = if (T.fst (evalExp e0 s)) < (T.fst (evalExp e1 (T.snd (evalExp e0 s)))) then (True :!: T.snd (evalExp e1 (T.snd (evalExp e0 s)))) else (False :!: T.snd (evalExp e1 (T.snd (evalExp e0 s))))
-evalExp (Gt e0 e1)    s = if (T.fst (evalExp e0 s)) > (T.fst (evalExp e1 (T.snd (evalExp e0 s)))) then (True :!: T.snd (evalExp e1 (T.snd (evalExp e0 s)))) else (False :!: T.snd (evalExp e1 (T.snd (evalExp e0 s))))
-evalExp (And p0 p1)   s = ((T.fst (evalExp p0 s)) && (T.fst (evalExp p1 (T.snd (evalExp p0 s)))) :!: T.snd (evalExp p1 (T.snd (evalExp p0 s))))
-evalExp (Or p0 p1)    s = (((T.fst (evalExp p0 s)) || (T.fst (evalExp p1 (T.snd (evalExp p0 s))))) :!: T.snd (evalExp p1 (T.snd (evalExp p0 s))))
-evalExp (Not p)       s = (not (T.fst (evalExp p s)) :!: T.snd (evalExp p s))
-evalExp (Eq e0 e1)    s = if (T.fst (evalExp e0 s)) == (T.fst (evalExp e1 (T.snd (evalExp e0 s)))) then (True :!: T.snd (evalExp e1 (T.snd (evalExp e0 s)))) else (False :!: T.snd (evalExp e1 (T.snd (evalExp e0 s))))
-evalExp (NEq e0 e1)   s = if (T.fst (evalExp e0 s)) /= (T.fst (evalExp e1 (T.snd (evalExp e0 s)))) then (True :!: T.snd (evalExp e1 (T.snd (evalExp e0 s)))) else (False :!: T.snd (evalExp e1 (T.snd (evalExp e0 s))))
+evalExp (Lt e0 e1)    s = let (n0 :!: s')  = evalExp e0 s
+                              (n1 :!: s'') = evalExp e1 s'
+                          in (n0 < n1 :!: s'')
+evalExp (Gt e0 e1)    s = let (n0 :!: s')  = evalExp e0 s
+                              (n1 :!: s'') = evalExp e1 s'
+                          in (n0 > n1 :!: s'')
+evalExp (And p0 p1)   s = let (b0 :!: s')  = evalExp p0 s
+                              (b1 :!: s'') = evalExp p1 s'
+                          in (b0 && b1 :!: s'')
+evalExp (Or p0 p1)    s = let (b0 :!: s')  = evalExp p0 s
+                              (b1 :!: s'') = evalExp p1 s'
+                          in ((b0 || b1) :!: s'')
+evalExp (Not p)       s = let (b :!: s') = evalExp p s
+                          in (not b :!: s')
+evalExp (Eq e0 e1)    s = let (n0 :!: s')  = evalExp e0 s
+                              (n1 :!: s'') = evalExp e1 s'
+                          in (n0 == n1 :!: s'')
+evalExp (NEq e0 e1)   s = let (n0 :!: s')  = evalExp e0 s
+                              (n1 :!: s'') = evalExp e1 s'
+                          in (not (n0 == n1) :!: s'')
