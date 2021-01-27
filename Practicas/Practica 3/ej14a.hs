5d12aa18db011c5fc6a7703487652cc1c4b0227e
vperezm <valeperezmog@gmail.com>
2021-01-27 15:26:34 -0300

    termino p3 ej14a, agrego teorias de tipos y polimorfismo, agrego practica 4

--- Practicas/Practica 3/ej14a.hs
+++ Practicas/Practica 3/ej14a.hs
@@ -15,10 +15,23 @@ isLeaf = \tree -> foldBin tree True (\a p q -> False)
 -- (ii)
 -- mapBin aplica la funcion argumento a todos los elementos de tipo a en el arbol
 mapBin :: (a -> b) -> BinTree a -> BinTree b
-mapBin = undefined
+-- mapBin f     Leaf    = Leaf
+-- mapBin f (Bin a t u) = Bin (f a) (mapBin f t) (mapBin f u)
+-- mapBin f tree = foldBin tree Leaf (\a t u -> (Bin (f a) t u))
+mapBin = \f tree -> foldBin tree Leaf (\a t u -> (Bin (f a) t u))
 
 -- (iii)
 -- heightBin devuelve la altura del arbol
+heightBin :: BinTree a -> Int
+-- heightBin     Leaf    = 1
+-- heightBin (Bin a t u) = 1 + max (heightBin t) (heightBin u)
+-- heightBin tree = foldBin tree 1 (\a h1 h2 -> 1 + max h1 h2)
+heightBin = \tree -> foldBin tree 1 (\a h1 h2 -> 1 + max h1 h2)
 
 -- (iv)
 -- mirrorBin devuelve el arbol espejo
+mirrorBin :: BinTree a -> BinTree a
+-- mirrorBin     Leaf    = Leaf
+-- mirrorBin (Bin a t u) = Bin a (mirrorBin u) (mirrorBin t)
+-- mirrorBin tree = foldBin tree Leaf (\a t u -> (Bin a u t))
+mirrorBin = \tree -> foldBin tree Leaf (\a t u -> (Bin a u t))
